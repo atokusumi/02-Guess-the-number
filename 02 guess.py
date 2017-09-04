4e5153f0c17043685e287a2635f00eb72f328c5b
atokusumi <31392375+atokusumi@users.noreply.github.com>
2017-09-04 19:16:51 -0400

    add strings and integers

--- 02 guess.py
+++ 02 guess.py
@@ -4,28 +4,40 @@ import sys
 import random
 assert sys.version_info >= (3,4), "This script requires at least Python 3.4"
 
+guessesTaken = 0
+
+print('Konnichiwa! What is your name?')
+myName = input()
 
 #generate a random integer between 1 and 20 (inclusive) and store it in the variable [number]
 number = random.randint(1,20)
 
+print('Nice to meet you ' + myName + ' , I am thinking of a number between 1 and 20.')
 
-
-#ask the user for a response and store it in the variable [guess]
-guess = input()
-
-
-
-#a try/except block is a great tool for programmers to be able to deal with errors. In this instance, it reports an error if the user enters something other than an integer
-try:
-	#convert the guess to an integer
-	guess = int(guess)
-
-
-	#check if the guess is less than the random number
-	if guess < number:
-		print('Too low!')
+for guessTaken in range(5):
+		print('Take a guess')
+		guess = input()
+		try:
+				guess = int(guess)
+				
+				if guess > number:
+					print('Guess is too high!')
+					
+				if guess < number:
+					print('Guess is too low!')
+					
+				if guess == number:
+						break
+						
 
 
 except ValueError:
 	print('Please enter a whole number.')
-
+	
+if guess == number:
+		guessesTaken = str(guessesTaken + 1)
+		print('Nice, ' + myName +'! You guessed my number in ' +guessesTaken + ' guesses!')
+		
+if guess != number:
+		number = str(number)
+		print('Sorry. THe number I was thinking of was ' + number +'.')
