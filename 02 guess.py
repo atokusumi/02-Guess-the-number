ffa104482328dd27c8df965f414e957bb18e9fe8
atokusumi <31392375+atokusumi@users.noreply.github.com>
2017-09-06 18:53:46 -0400

    add variable

--- 02 guess.py
+++ 02 guess.py
@@ -12,7 +12,7 @@ myName = input('Konnichiwa! What is your name?')
 number = random.randint(1,20)
 print('Nice to meet you ' + myName + ' , I am thinking of a number between 1 and 20.')
 
-for chances in range(10):
+for chances in range(5):
 		guess = input('take a guess:')
 		try:
 				guess = int(guess)
@@ -26,13 +26,12 @@ for chances in range(10):
 				elif guess == number:
 						print('Yay! You guessed it right!')
 						break
-
+		while guess != number:
+				print('that was your ' + chances+1 + ' guess')
+				
 		except ValueError:
 			print('Please enter a whole number.')
 		
-if chance == range[10] and guess != number:
-		print('Nope! Sorry!! The number I was thinking was:')
-		input(number)
 		
 
 		
