8f98b53cc9782d4ba1afe596e85c7ca93a03b0f4
atokusumi <31392375+atokusumi@users.noreply.github.com>
2017-09-06 17:50:54 -0400

    change veriables

--- 02 guess.py
+++ 02 guess.py
@@ -4,36 +4,35 @@ import sys
 import random
 assert sys.version_info >= (3,4), "This script requires at least Python 3.4"
 
-guessesTaken = 0
+chances = 0
 
-print('Konnichiwa! What is your name?')
-myName = input()
+myName = input('Konnichiwa! What is your name?')
 
 #generate a random integer between 1 and 20 (inclusive) and store it in the variable [number]
 number = random.randint(1,20)
 print('Nice to meet you ' + myName + ' , I am thinking of a number between 1 and 20.')
 
-for guessTaken in range(6):
-		print('Take a guess')
-		guess = input()
+for chances in range(10):
+		guess = input('take a guess:')
 		try:
 				guess = int(guess)
 				
 				if guess > number:
 					print('Guessed too high!')
 					
-				if guess < number:
+				elif guess < number:
 					print('Guessed too low!')
 					
-				if guess == number:
+				elif guess == number:
+					print('Yay! You guessed it right!')
 						break
 
 		except ValueError:
 			print('Please enter a whole number.')
 		
 if guess == number:
-		guessesTaken = str(guessesTaken + 1)
-		print('Nice, ' + myName +'! You guessed my number in ' + guessesTaken + ' guesses!')
+		chances = str(chances+1)
+		print('Nice, ' + myName +'! You guessed my number in ' + chances+1 + ' guesses!')
 		
 if guess != number:
 		number = str(number)
