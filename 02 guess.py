2ca9d61c62d8f16f834e433210a29241f107f691
atokusumi <31392375+atokusumi@users.noreply.github.com>
2017-09-06 22:59:30 -0400

    change and add variables

--- 02 guess.py
+++ 02 guess.py
@@ -5,6 +5,7 @@ import random
 assert sys.version_info >= (3,4), "This script requires at least Python 3.4"
 
 chances = 0
+maxChances = 5
 
 myName = input('Konnichiwa! What is your name?')
 
@@ -12,31 +13,32 @@ myName = input('Konnichiwa! What is your name?')
 number = random.randint(1,20)
 print('Nice to meet you ' + myName + ' , I am thinking of a number between 1 and 20.')
 
-while chances < 5:
+for chance in range(maxChances):
 		guess = input('take a guess:')
-		guess = int(guess)		
-		
-		chances = chances + 1
+		try:
+				guess = int(guess)
 				
-		if guess > number:
-				print('Guessed too high!')
-					
-		if guess < number:
-				print('Guessed too low!')
-					
-		if guess == number:
-				break
+				if guess < number:
+						print('Guessed too low')
+				
+				if guess > number:
+						print('Guessed too high!')
+						
+				if guess == number:
+						break
+						print('That is correct!')
 			
 		except ValueError:
 			print('Please enter a whole number.')
 			
 if guess == number:
-		chances = str(chances)
-	    print('Good job, ' + myName + '! You guessed my number in ' + chances + ' guesses!')
+		chances = str(chances + 1)
+		print('Yay! You guessed teh number in ' + chances + ' guesses!')	
 
 if guess != number:
 		number = str(number)
-		print('Nope. The number I was thinking of was' + number)
+		print('Nope! The number I was thinking of was ' + number + '.')
+		
 		
 
 			
